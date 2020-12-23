b32662fca5588989ce0f15cedcbea36395ff74d6
Adrian Ciura <aciura@gmail.com>
2020-12-23 23:54:19 +0100

    Change api server port to 4000

--- node-api-server/src/app.ts
+++ node-api-server/src/app.ts
@@ -13,7 +13,7 @@ import { OrdersRoutes } from './orders/orders.routes.config'
 
 const app: express.Application = express()
 const server: http.Server = http.createServer(app)
-const port: Number = 3000
+const port: Number = 4000
 const routes: Array<CommonRoutesConfig> = []
 const debugLog: debug.IDebugger = debug('app')
 
